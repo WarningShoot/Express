0bccab9159efdee0df381b7fe90275aa971f810e
Damian Zajda <damian.zajda@unilodz.eu>
2020-04-15 21:49:13 +0200

    obsługa newsów

--- models/news.js
+++ models/news.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const newsSchema = new Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
+    title: {type: String, required: [true, 'Podaj tytuł']},
+    description: {type: String, required: [true, 'Podaj treść newsa']},
     date: { type: Date, default: Date.now}
 })
 
--- routes/admin.js
+++ routes/admin.js
@@ -13,12 +13,41 @@ router.all('*', (req, res, next) => {
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
+  News.find({}, (err, data) => {
 
-  res.render('admin/index', { title: 'Admin' });
+    res.render('admin/index', { title: 'Admin', data });
+  });
 });
 
 router.get('/news/add', (req, res) => {
-  res.render('admin/news-form', { title: 'Form' });
+  res.render('admin/news-form', { title: 'Dodaj news', body: {}, errors: {}});
+})
+
+router.post('/news/add', (req, res, next) => {
+  const body = req.body;
+  const newsData = new News(body);
+
+  const errors = newsData.validateSync();
+  console.log(errors);
+  
+
+  newsData.save((err) => {
+    if(err)
+    {
+      res.render('admin/news-form', { title: 'Dodaj news', errors, body });
+      return;
+    }
+    res.redirect('/admin');
+
+    
+  });
+  
+})
+
+router.get('/news/delete/:id', (req, res) => {
+  News.findByIdAndDelete(req.params.id, (err) => {
+    res.redirect('/admin');
+  })
 })
 
 module.exports = router;
