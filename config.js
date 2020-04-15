d9faeca9412319ae6c53d458fc1453c1a3d22db1
Damian Zajda <damian.zajda@unilodz.eu>
2020-04-15 20:51:08 +0200

    a lot of changes

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://admin:skarbnica1@cluster0-nusq8.azure.mongodb.net/test?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news');
+
+router.all('*', (req, res, next) => {
+  if(!req.session.admin) {
+    res.redirect('login')
+    return;
+  }
+
+  next();
+});
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('admin', { title: 'Admin' });
+
+  res.render('admin/index', { title: 'Admin' });
 });
 
-
+router.get('/news/add', (req, res) => {
+  res.render('admin/news-form', { title: 'Form' });
+})
 
 module.exports = router;
