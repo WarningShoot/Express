c88a3c8540aa77dfd9b92158636116bc3fb218c0
Damian Zajda <damian.zajda@unilodz.eu>
2020-04-15 18:28:15 +0200

    add new files and add global layout

---
+++ routes/news.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('news', { title: 'News' });
+});
+
+
+
+module.exports = router;
